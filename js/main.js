9a57b49f891f12dc2e855a0cc1cdf9d215bcca33
David Sanchez <DavidSanchez@Hugos-MacBook-Pro.local>
2023-06-16 11:54:04 -0600

    Tesco Stock added

--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@ import {
     hightGraph2,
     volumeGraph2,
     lowGraph2,
+    highGraph3,
+    volumeGraph3,
+    lowGraph3,
     viewDataBtn
 } from './modules/DOMElements.js';
 
@@ -18,17 +21,24 @@ const myChart = async () => {
         ('https://api.polygon.io/v2/aggs/ticker/MU/range/1/day/2023-01-01/2023-06-30?apiKey=ukpp9BZvK1spdexOWLGB223Yp8q5kz0p');
         let data2raw = await apiStock
         ('https://api.polygon.io/v2/aggs/ticker/IBM/range/1/day/2023-01-01/2023-06-30?apiKey=ukpp9BZvK1spdexOWLGB223Yp8q5kz0p');
+        let data3raw = await apiStock
+        ('https://api.polygon.io/v2/aggs/ticker/UK/range/1/day/2023-01-01/2023-06-30?apiKey=ukpp9BZvK1spdexOWLGB223Yp8q5kz0p');
         console.log(data1raw.results);
         console.log(data2raw.results);
+        console.log(data3raw.results);
         let data1 = polygondata(data1raw.results);
         let data2 = polygondata(data2raw.results);
-        console.log(data1, data2);
+        let data3 = polygondata(data3raw.results);
+        console.log(data1, data2, data3);
         stockChart(data1.date, data1.hight, hightGraph, "High Price" );
         stockChart(data1.date, data1.volume, volumeGraph, "Volumen Per Day" );
         stockChart(data1.date, data1.low, lowGraph, "Low Price" );
         stockChart(data2.date, data2.hight, hightGraph2, "High Price 2" );
         stockChart(data2.date, data2.volume, volumeGraph2, "Volumen Per Day");
         stockChart(data2.date, data2.low, lowGraph2, "Low Price");
+        stockChart(data3.date, data3.hight, highGraph3, "High Price 2" );
+        stockChart(data3.date, data3.volume, volumeGraph3, "Volumen Per Day");
+        stockChart(data3.date, data3.low, lowGraph3, "Low Price");
         //console.log(dataStock);
         //stockChart(dataApi.results, lowGraph, "Low Price");
     } catch (error) {
--- js/modules/DOMElements.js
+++ js/modules/DOMElements.js
@@ -12,6 +12,12 @@ export const volumeGraph2 = document.getElementById('volumeChart2');
 
 export const lowGraph2 = document.getElementById('lowChart2');
 
+export const highGraph3 = document.getElementById('highChart3');
+
+export const volumeGraph3 = document.getElementById('volumeChart3');
+
+export const lowGraph3 = document.getElementById('lowChart3');
+
 
 
 export const viewDataBtn = document.getElementById('viewDataBtn');
